0ccd28bf66028665b331a2ed7c31540547be99fa
Jeet Panchal <jeetbpanchal@gmail.com>
2024-10-15 18:10:39 -0400

    Adding new View Files

---
+++ HuliPizzaApp/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  HuliPizzaApp
+//
+//  Created by Jeet Panchal on 2024-10-15.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
---
+++ HuliPizzaApp/OrderView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderView.swift
+//  HuliPizzaApp
+//
+//  Created by Jeet Panchal on 2024-10-15.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderView()
+}
