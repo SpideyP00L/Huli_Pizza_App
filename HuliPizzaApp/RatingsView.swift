515de9e0ce11f9db7024b651dfd06e75c49be578
Jeet Panchal <jeetbpanchal@gmail.com>
2024-10-15 18:53:35 -0400

    Adding Rating View

---
+++ HuliPizzaApp/RatingsView.swift
@@ -0,0 +1,26 @@
+//
+//  RatingsView.swift
+//  HuliPizzaApp
+//
+//  Created by Jeet Panchal on 2024-10-15.
+//
+
+import SwiftUI
+
+struct RatingsView: View {
+    
+    var rating : Int
+    
+    var body: some View {
+        
+        HStack {
+            ForEach (1...6, id: \.self) { circle in
+                Image(systemName: (circle <= rating) ? "fork,knife.circle.fill" : "circle")
+            }
+        }
+    }
+}
+
+#Preview {
+    RatingsView(rating: 4)
+}
