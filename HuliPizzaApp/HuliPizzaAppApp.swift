99c2e5d3adf0a226a2e98000d016ced1a8e6b659
Jeet Panchal <jeetbpanchal@gmail.com>
2024-10-15 13:31:56 -0400

    Initial Commit

---
+++ HuliPizzaApp/HuliPizzaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaAppApp.swift
+//  HuliPizzaApp
+//
+//  Created by Jeet Panchal on 2024-10-15.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HuliPizzaAppTests/HuliPizzaAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaAppTests.swift
+//  HuliPizzaAppTests
+//
+//  Created by Jeet Panchal on 2024-10-15.
+//
+
+import Testing
+@testable import HuliPizzaApp
+
+struct HuliPizzaAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
