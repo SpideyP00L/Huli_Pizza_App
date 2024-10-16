982854ac5cf9993d0530bfd200444224003f5eee
Jeet Panchal <jeetbpanchal@gmail.com>
2024-10-15 20:20:11 -0400

    Cleaning Up the Views

--- HuliPizzaApp/MenuItemView.swift
+++ HuliPizzaApp/MenuItemView.swift
@@ -10,21 +10,32 @@ import SwiftUI
 struct MenuItemView: View {
     var body: some View {
         VStack {
-            if let image = UIImage(named : "0_lg") {
-                Image(uiImage: image)
-                    .cornerRadius(10)
-                    .shadow(color: .teal,  radius: 5, x : 8, y : 8)
-            } else {
-                Image("surfboard_lg")
-            }
             
-            VStack (alignment: .leading) {
-                Text("Margherita")
+            HStack {
+                Text("Margherita Huli Pizza")
                     .font(.title)
                     .fontWeight(.semibold)
-                    .foregroundStyle(.thickMaterial)
+                    .foregroundStyle(.ultraThickMaterial)
                     .padding(.leading)
-                    .background(.linearGradient(colors: [Color("Surf"), Color("Sky").opacity(0.1)], startPoint: .leading, endPoint: .trailing), in: Capsule())
+                
+                if let image = UIImage(named : "0x_lg") {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .padding([.top, .bottom], 5)
+                        .cornerRadius(15)
+                } else {
+                    Image("surfboard_lg")
+                        .resizable()
+                        .scaledToFit()
+                        .rotationEffect(.degrees(180))
+                }
+            }
+            .background(.linearGradient(colors: [Color("Surf"), Color("Sky").opacity(0.1)], startPoint: .leading, endPoint: .trailing), in: Capsule())
+            .shadow(color: .teal,  radius: 5, x : 8, y : 8)
+            
+            VStack (alignment: .leading) {
+                
                 
                 ScrollView {
                     Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pharetra mi nec odio ornare, nec ornare dui tristique. Vestibulum volutpat vestibulum est, ac aliquam quam euismod sit amet. Nam auctor, purus sit amet porttitor convallis, diam augue sodales velit, sit amet tempus neque odio in elit. Morbi at est elit.")
